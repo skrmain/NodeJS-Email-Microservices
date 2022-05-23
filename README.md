1c35d5190574274c70867d27619e2c955c9296a2
Sachin Kumar Rajput <skr571999@gmail.com>
2022-05-23 14:17:30 +0530

    setup aws sdk

---
+++ README.md
@@ -0,0 +1,5 @@
+## nodejs-dynamodb-demo
+
+## Notes
+
+- https://www.npmjs.com/package/@aws-sdk/client-dynamodb
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+(async () => {
+    const client = new DynamoDB({ region: "ap-south-1", endpoint: "http://localhost:8000" });
+    try {
+        const results = await client.listTables({});
+
+        console.log(results.TableNames);
+    } catch (err) {
+        console.error(err);
+    }
+})();
