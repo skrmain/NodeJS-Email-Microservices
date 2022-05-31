2b27d02f9e753a9fbbcba4963186282fe30bfe6a
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-05-31 05:27:26 +0000

    correct Expose port, update the app structure

--- product-server/src/index.ts
+++ product-server/src/index.ts
@@ -1,36 +1,14 @@
 import { config } from "dotenv";
 config({ path: "src/.env" });
-import "reflect-metadata";
 
-import express from "express";
-import cors from "cors";
-
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-
-import { PORT } from "./constants";
 import { connectMySQL } from "./utils";
-
-import { SwaggerConfig } from "./config/swagger";
-import BaseRoute from "./api/routes/index.route";
+import { PORT } from "./constants";
+import app from "./server";
 
 (async () => {
     try {
-        const app = express();
-
         await connectMySQL();
         console.log("DB Connected");
-
-        app.use(express.json());
-        app.use(cors());
-
-        app.get("/", (req, res) => {
-            res.send({ message: "Server is running" });
-        });
-        app.use("/api", BaseRoute);
-
-        const swaggerDocs = swaggerJSDoc(SwaggerConfig);
-        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
         app.listen(PORT, () => {
             console.log(`Server running on ${PORT}`);
