d7478e8c8f521672a7037c45c8b5622d7bdc30a8
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-12-04 12:43:41 +0000

    [combine] combine dynamodb code

--- README.md
+++ README.md
@@ -39,3 +39,31 @@ await this.client.set('name', 'Admin', {
   EX: 10,
 });
 ```
+
+
+## DynamoDB
+
+### Prerequisites
+
+- DynamoDB Local - Java, AWS CLI
+- NodeJS
+
+### Notes
+
+> DynamoDB
+
+- Developer Guide: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Introduction.html
+  - Install: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html#DynamoDBLocal.DownloadingAndRunning.title
+  - Doc AWS CLI + DynamoDB: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.CLI.html
+- API References: https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/Welcome.html
+- https://askubuntu.com/questions/25347/what-command-do-i-need-to-unzip-extract-a-tar-gz-file
+- Java Install: - https://ubuntu.com/tutorials/install-jre#1-overview
+- AWS CLI Install: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html#cliv2-linux-install
+
+> Other
+
+- https://www.npmjs.com/package/@aws-sdk/client-dynamodb
+- https://www.npmjs.com/package/@aws-sdk/lib-dynamodb
+- CRUD - https://devapt.com/dynamodb-nodejs-crud
+- CRUD API - https://codezup.com/integrate-aws-dynamodb-with-node-js-crud-operations/
+- https://youtu.be/2k2GINpO308
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import redisProductModel from './database/redis.model';
-
+import redisProductModel from './database/redis.database';
 import { ProductSchema } from './types';
+import { listTables } from './utils/dynamodb.util';
 
 (async () => {
     const p1: ProductSchema = {
@@ -29,3 +29,43 @@ import { ProductSchema } from './types';
     // console.log("B", product2);
     // console.log(await redisProductModel.deleteProduct("1"));
 })();
+
+// - DynamoDB
+
+export const run = async () => {
+    try {
+        // const result = await createTable();
+        // console.log("Table Created", result.TableDescription?.TableName);
+
+        const dbTables = await listTables();
+        console.log('DB Tables ', dbTables.TableNames);
+
+        // for (const product of Products) {
+        //   const savedItem = await updateOrAddItem({ ...product });
+        //   console.log("Saved Item: ", savedItem);
+        // }
+
+        // const item = await getAllItems();
+        // console.log("Item ", item.Items);
+
+        // const deletedItem = await deleteItem("m1v930", "1653368519448");
+        // console.log("Deleted Item ", deletedItem);
+
+        // // - Query Command
+        // const result = await ddbDocClient.send(
+        //   new QueryCommand({
+        //     TableName: TABLE_NAME,
+        //     ExpressionAttributeValues: {
+        //       ":id": "5q6rk5",
+        //       ":de": "16533685194",
+        //     },
+        //     KeyConditionExpression: "Id = :id and dateEdited >= :de",
+        //     // ProjectionExpression: "",
+        //   })
+        // );
+        // console.log("Result ", result);
+    } catch (err) {
+        console.log('Error', err);
+    }
+};
+// run();
