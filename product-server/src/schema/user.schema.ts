81454dc0710b971f93271986d269d275ac14e64e
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2023-01-24 16:58:38 +0000

    [refactor] to email microservice
    - delete CRUD operation from product-service
    - rename product-service to server

--- product-server/src/schema/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-import { UserRole } from "../entity/user.entity";
-
-export const UserSchema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30),
-    email: Joi.string().email(),
-    role: Joi.string().valid(UserRole),
-});
--- server/src/shared/utils.ts
+++ server/src/shared/utils.ts
@@ -1,32 +1,8 @@
-import { join } from "path";
-import { createConnection } from "typeorm";
 import { Connection } from "amqplib";
 import { connect } from "amqplib/callback_api";
 
 const RMQ_URL = process.env.RMQ_URL || "amqp://localhost";
 const RMQ_QUEUE = process.env.RMQ_QUEUE || "development";
-const MYSQL_HOSTNAME = process.env.MYSQL_HOSTNAME || "localhost";
-const MYSQL_PORT = parseInt(process.env.MYSQL_PORT || "3306");
-const MYSQL_USER = process.env.MYSQL_USER || "root";
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "root";
-const MYSQL_DATABASE = process.env.MYSQL_DATABASE || "test";
-
-export const connectMySQL = () => {
-    const entityPathTS = join(__dirname, "../entity/*.ts");
-    const entityPathJS = join(__dirname, "../entity/*.js");
-
-    return createConnection({
-        type: "mysql",
-        host: MYSQL_HOSTNAME,
-        port: MYSQL_PORT,
-        username: MYSQL_USER,
-        password: MYSQL_PASSWORD,
-        database: MYSQL_DATABASE,
-        entities: [entityPathTS, entityPathJS],
-        synchronize: false,
-        logging: false,
-    });
-};
 
 export const getRMQConnection = () => {
     return new Promise<Connection>((resolve: any, reject) => {
@@ -67,7 +43,6 @@ export const sendMailMessageInQueue = async (data: any) => {
         return true;
     } catch (error) {
         console.log("Error : ", error);
-
         return false;
     }
 };
