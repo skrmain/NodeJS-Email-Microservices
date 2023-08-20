901c3349c2b5b5c49579d88e56dae529725d265c
Sachin Kumar Rajput <skr571999@gmail.com>
2023-08-20 11:42:38 +0530

    add: graphql example

---
+++ src/examples/exprerss-graphql-server/index.ts
@@ -0,0 +1,53 @@
+// const main = async () => {
+//   try {
+//     console.log('Hello World');
+//   } catch (error) {
+//     console.error('[error]', error);
+//   }
+// };
+
+// main();
+
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+// // Construct a schema, using GraphQL schema language
+// const schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// const root = {
+//     hello: () => 'Hello world2!',
+// };
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+    },
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true }));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
